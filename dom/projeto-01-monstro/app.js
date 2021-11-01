3355f97977532f726530e980ffd4aae350ed8ceb
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-11-01 13:23:13 -0300

    69. Adicionando o Registro de Log #01

--- dom/projeto-01-monstro/app.js
+++ dom/projeto-01-monstro/app.js
@@ -3,7 +3,8 @@ new Vue({
 	data: {
 		running: false,
 		playerLife: 100,
-		monsterLife: 100
+		monsterLife: 100,
+		logs: []
 	},
 	computed: {
 		hasResult() {
@@ -17,15 +18,20 @@ new Vue({
 			this.monsterLife = 100
 		},
 		attack(especial) {
-			this.hurt('monsterLife', 5, 10, especial)
-			this.hurt('playerLife', 7, 12, false)
+			this.hurt('monsterLife', 5, 10, especial, 'Player', 'Monstro', 'player')
+
+			if (this.monsterLife > 0) {
+				this.hurt('playerLife', 7, 12, false, 'Monstro', 'Player', 'monster')
+			}
 		},
-		hurt(atr, min, max, especial) {
+		hurt(atr, min, max, especial, source, target, cls) {
 			const plus = especial ? 5 : 0
 
 			const hurt = this.getRandom(min + plus, max + plus)
 
 			this[atr] = Math.max(this[atr] - hurt, 0)
+
+			this.registerLog(`${source} atingiu ${target} com ${hurt}`, cls)
 		},
 		healAndHurt() {
 			this.heal(5, 10)
@@ -40,6 +46,9 @@ new Vue({
 			const value = Math.random() * (max - min) + min
 
 			return Math.round(value)
+		},
+		registerLog(text, cls) {
+			this.logs.unshift({ text, cls })
 		}
 	},
 	watch: {
