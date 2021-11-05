9d29d8d3b6c85d79b5f7ae6d27830ed366fd362e
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-11-05 09:26:12 -0300

    96. Entendendo arquivos .vue

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<div id="app">
+		{{ titulo }}
+
+		<button @click="titulo += '#'">Alterar título</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			titulo: 'Teste data usando VueJS'
+		}
+	}
+}
+</script>
+
+<style>
+#app {
+	background-color: brown;
+	color: white;
+}
+</style>
