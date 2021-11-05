c70a93e75cf998b0e82cd2b204645679358edd23
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-11-05 15:35:09 -0300

    109. Usando componentes

---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="contadores">
+		<h1>Contadores</h1>
+
+		<app-contador v-for="contador in 5" :key="contador" />
+	</div>
+</template>
+
+<script>
+import ContadorVue from './Contador.vue'
+
+export default {
+	components: {
+		'app-contador': ContadorVue
+	}
+}
+</script>
+
+<style></style>
