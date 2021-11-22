265464fe4228202493e4cd9f9a6cf2327bf6bea5
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-11-22 08:44:01 -0300

    Resposta: minha versão

--- formularios/formulario-desafio/src/App.vue
+++ formularios/formulario-desafio/src/App.vue
@@ -2,26 +2,66 @@
 	<div id="app">
 		<h1>Formulário Desafio</h1>
 		<div class="conteudo">
-			<form class="painel">
+			<!-- Exercicio 02 -->
+			<!-- Só mostrar o fomulário de não tiver sido submetido -->
+			<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
+			<form v-if="!submitted" class="painel">
 				<div class="cabecalho">Formulário</div>
 				<!-- Exercicio 01 -->
 				<!-- Criar uma formulário de registro -->
 				<!-- Nome completo (Nome e Sobrenome) -->
-				<!-- Email -->
-				<!-- Senha -->
-				<!-- Armazenar Dados? (Sim/Não) -->
+				<Rotulo nome="Nome">
+					<input id="name" type="text" placeholder="Nome" v-model="user.name">
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<input id="surname" type="text" placeholder="Sobrenome"  v-model="user.surname">
+				</Rotulo>
 
-				<!-- Exercicio 02 -->
-				<!-- Só mostrar o fomulário de não tiver sido submetido -->
-				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
-				
+				<!-- Email -->
+				<Rotulo nome="E-mail">
+					<input id="email" type="email" placeholder="E-mail"  v-model="user.email">
+				</Rotulo>
+
+				<!-- Senha -->
+				<Rotulo nome="Senha">
+					<input id="password" type="password" placeholder="Senha"  v-model="user.password">
+				</Rotulo>
+
+				<!-- Armazenar Dados? (Sim/Não) -->
+				<Rotulo nome="Armazenar dados?">
+					<span class="mr-4">
+						<input type="radio" value="Sim" v-model="shouldStoreData"> Sim
+					</span>
+					<span class="mr-4">
+						<input type="radio" value="Não" v-model="shouldStoreData"> Não
+					</span>
+				</Rotulo>
+
+				<button @click="submitForm" type="button">Submeter formulário</button>
+			</form>
+
+			<div v-else class="painel">
 				<!-- Exercicio 03 -->
 				<!-- Crie um componente personalizado NomeCompleto -->
 				<!-- Esse componente deve receber Nome e Sobrenome -->
-			</form>
-			<div class="painel">
-				<div class="cabecalho">Resultado</div>
+				<Rotulo nome="Nome completo">
+					<NomeCompleto :name="user.name" :surname="user.surname" />
+				</Rotulo>
 
+				<!-- Email -->
+				<Rotulo nome="E-mail">
+					<output>{{ user.email }}</output>
+				</Rotulo>
+
+				<!-- Senha -->
+				<Rotulo nome="Senha">
+					<output>{{ user.password }}</output>
+				</Rotulo>
+
+				<!-- Armazenar Dados? (Sim/Não) -->
+				<Rotulo nome="Armazenar dados?">
+					<output>{{ shouldStoreData }}</output>
+				</Rotulo>
 			</div>
 		</div>
 	</div>
@@ -29,17 +69,34 @@
 
 <script>
 import Rotulo from './components/Rotulo.vue'
+import NomeCompleto from './components/NomeCompleto.vue'
 
 export default {
 	name: 'app',
-	components: { Rotulo }
+	components: { Rotulo, NomeCompleto },
+	data() {
+		return {
+			user: {
+				name: '',
+				surname: '',
+				email: '',
+				password: ''
+			},
+			shouldStoreData: 'Sim',
+			submitted: false
+		}
+	},
+	methods: {
+		submitForm() {
+			this.submitted = true
+		}
+	}
 }
 </script>
 
 <style>
-
 body {
-	background-color: #ECECEC;
+	background-color: #ececec;
 }
 
 #app {
@@ -59,16 +116,16 @@ body {
 
 .painel {
 	flex: 1;
-	background: #FFF;
+	background: #fff;
 	margin: 0px 10px;
 	padding: 20px;
-	border: 1px solid #AAA;
+	border: 1px solid #aaa;
 	border-radius: 5px;
 }
 
 .painel .cabecalho {
 	width: 100%;
-	background-color: #DDD;
+	background-color: #ddd;
 	padding: 10px 0px;
 	border-radius: 5px;
 	font-size: 1.4rem;
@@ -80,8 +137,8 @@ body {
 	padding: 10px 20px;
 	font-size: 1.4rem;
 	border-radius: 5px;
-	color: #FFF;
-	background-color: #2196F3;
+	color: #fff;
+	background-color: #2196f3;
 }
 
 #app h1 {
---
+++ formularios/formulario-desafio/src/components/NomeCompleto.vue
@@ -0,0 +1,18 @@
+<template>
+  <output>{{ name }} {{ surname }}</output>
+</template>
+
+<script>
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true
+		},
+		surname: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
