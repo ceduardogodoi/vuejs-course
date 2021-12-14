033d92a60bd7c691e63b48a83e765c675e7b17d1
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-12-13 22:50:42 -0300

    236. Configuração Global do Axios

---
+++ http/http-exercicios/src/plugins/axios.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'https://curso-vue-a2b9f-default-rtdb.firebaseio.com/'
+
+Vue.use({
+	install(Vue) {
+		Vue.prototype.$http = axios
+	}
+})
