f04486a05bfa0e8e0ce73e10ab6d5e2fff6b03a6
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-12-14 09:07:56 -0300

    242. Interceptando Requisições

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -6,5 +6,13 @@ axios.defaults.baseURL = 'https://curso-vue-a2b9f-default-rtdb.firebaseio.com/'
 Vue.use({
 	install(Vue) {
 		Vue.prototype.$http = axios
+
+		Vue.prototype.$http.interceptors.request.use(config => {
+			console.log('request interceptor:', config.method)
+			// if (config.method === 'post') {
+			// 	config.method = 'put'
+			// }
+			return config
+		})
 	}
 })
