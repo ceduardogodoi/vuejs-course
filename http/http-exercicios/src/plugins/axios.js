24702d0855704c5e43ee1d58f269f570d4176d1b
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-12-13 23:05:16 -0300

    238. Criando Formulário

--- http/http-exercicios/src/plugins/axios.js
+++ http/http-exercicios/src/plugins/axios.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-// axios.defaults.baseURL = 'https://curso-vue-a2b9f-default-rtdb.firebaseio.com/'
+axios.defaults.baseURL = 'https://curso-vue-a2b9f-default-rtdb.firebaseio.com/'
 
 Vue.use({
 	install(Vue) {
-		// Vue.prototype.$http = axios
-		Vue.prototype.$http = axios.create({
-			baseURL: 'https://curso-vue-a2b9f-default-rtdb.firebaseio.com/'
-		})
-
-		Vue.prototype.$http2 = axios.create({
-			baseURL: 'https://jsonplaceholder.typicode.com/'
-		})
+		Vue.prototype.$http = axios
 	}
 })
