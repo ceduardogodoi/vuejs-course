6e5ba4edaf95f8dae1d54a0d818fea0c64a5627b
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-12-20 08:56:39 -0300

    260. Configurando Rotas Filhas (Rotas Aninhadas)

---
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="usuario-editar">
+    <h3>Usuário editar</h3>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -2,14 +2,30 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Inicio from './components/Inicio.vue'
+
 import Usuario from './components/usuario/Usuario.vue'
+import UsuarioLista from './components/usuario/UsuarioLista.vue'
+import UsuarioEditar from './components/usuario/UsuarioEditar.vue'
+import UsuarioDetalhe from './components/usuario/UsuarioDetalhe.vue'
 
 Vue.use(Router)
 
 export default new Router({
 	mode: 'history',
 	routes: [
-		{ path: '/', component: Inicio },
-		{ path: '/usuario/:id', component: Usuario, props: true }
+		{
+			path: '/',
+			component: Inicio
+		},
+		{
+			path: '/usuario',
+			component: Usuario,
+			props: true,
+			children: [
+				{ path: '', component: UsuarioLista },
+				{ path: ':id', component: UsuarioDetalhe, props: true },
+				{ path: ':id/editar', component: UsuarioEditar, props: true }
+			]
+		}
 	]
 })
