e302580c36ea8ee4d5711e4a858b6492e0dc1539
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-12-20 21:04:36 -0300

    261. Navegando para Rotas Aninhadas

--- rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ rotas/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="usuario-editar">
     <h3>Usuário editar</h3>
+    <p>
+      <strong>Código: </strong>{{ id }}
+    </p>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+	props: ['id']
+}
 </script>
 
 <style>
