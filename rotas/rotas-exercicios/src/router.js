25f10ab28ac5330ce150cc771438c49bcce88fc6
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2021-12-22 21:38:50 -0300

    270. Controlando o Comportamento de Rolagem (Scroll)

--- rotas/rotas-exercicios/src/router.js
+++ rotas/rotas-exercicios/src/router.js
@@ -15,6 +15,19 @@ Vue.use(Router)
 
 export default new Router({
 	mode: 'history',
+	scrollBehavior(to, from, savedPosition) {
+		if (savedPosition) {
+			return savedPosition
+		}
+
+		if (to.hash) {
+			return {
+				selector: to.hash
+			}
+		}
+
+		return { x: 0, y: 0 }
+	},
 	routes: [
 		{
 			path: '/',
